4c638e25bab7a7f47cd9b542ae647e9c79bc9c83
Martin Terrier <martin.terrier@gmail.com>
2024-05-21 20:34:04 +0200

    Heroku setup tests.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ServicesModule } from './services/services.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as process from 'node:process';
+import 'dotenv/config';
 
 @Module({
   imports: [
     ServicesModule,
     TypeOrmModule.forRoot({
+      url: process.env.DATABASE_URL,
+      // url: 'postgres://postgres:projetbroceliande@localhost:5432/zoo-broceliande',
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'projetbroceliande',
-      database: 'zoo-broceliande',
+      // host: 'localhost',
+      // port: 5432,
+      // username: 'postgres',
+      // password: 'projetbroceliande',
+      // database: 'zoo-broceliande',
       autoLoadEntities: true,
       synchronize: true,
     }),
