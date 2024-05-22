180543ec1b4292f0ccc5ee3e89382b054cc1c22c
Martin Terrier <martin.terrier@gmail.com>
2024-05-22 10:21:55 +0200

    ValidateComment, RejectComment and GetCommentsByStatus methods added.

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseBoolPipe,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentDto } from './dto/commentDto';
 
@@ -6,8 +16,23 @@ import { CommentDto } from './dto/commentDto';
 export class CommentsController {
   constructor(private commentsService: CommentsService) {}
 
+  @Get('/:status')
+  async getCommentsByStatus(@Param('status', ParseBoolPipe) status: boolean) {
+    return await this.commentsService.getComments(status);
+  }
+
   @Post()
   async createComment(@Body() commentDto: CommentDto) {
     return await this.commentsService.createComment(commentDto);
+  }
+
+  @Patch('/:id')
+  async validateComment(@Param('id', ParseIntPipe) id: number) {
+    return await this.commentsService.displayComment(id);
+  }
+
+  @Delete('/:id')
+  async rejectComment(@Param('id', ParseIntPipe) id: number) {
+    await this.commentsService.deleteComment(id);
   }
 }
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CommentsRepository } from './comments.repository';
 import { CommentDto } from './dto/commentDto';
 
@@ -6,7 +6,27 @@ import { CommentDto } from './dto/commentDto';
 export class CommentsService {
   constructor(private commentsRepository: CommentsRepository) {}
 
+  async getComments(displayStatus: boolean) {
+    return await this.commentsRepository.findBy({ isDisplayed: displayStatus });
+  }
+
   async createComment(commentDto: CommentDto) {
     return await this.commentsRepository.createComment(commentDto);
+  }
+
+  async displayComment(id: number) {
+    const commentToDisplay = await this.commentsRepository.findOneBy({ id });
+
+    if (!commentToDisplay) {
+      throw new NotFoundException(`Comment with ID ${id} not found.`);
+    }
+
+    commentToDisplay.isDisplayed = true;
+    await this.commentsRepository.save(commentToDisplay);
+    return commentToDisplay;
+  }
+
+  async deleteComment(id: number) {
+    await this.commentsRepository.delete(id);
   }
 }
